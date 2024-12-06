9f17e89beb75f940a8d0a69cae3805161740f25e
NKulbaka <nikita.kulbaka@mail.ru>
2024-12-06 21:57:12 +0300

    Converted to Kotlin

--- app/src/main/java/com/example/helloandriod/MainActivity.kt
+++ app/src/main/java/com/example/helloandriod/MainActivity.kt
@@ -1,47 +1,43 @@
-package com.example.helloandriod;
+package com.example.helloandriod
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+class MainActivity : AppCompatActivity() {
+    private lateinit var greetingButton: Button
+    private lateinit var greetingTextView: TextView
 
-public class MainActivity extends AppCompatActivity {
-    private static final int SHOW_OTHER_ACTIVITY = 1;
-    private Button greetingButton;
-    private TextView greetingTextView;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        greetingButton = findViewById(R.id.greetingButton)
+        greetingTextView = findViewById(R.id.greetingTextView)
 
-        greetingButton = findViewById(R.id.greetingButton);
-
-        greetingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String greeting = "Привет";
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("greeting", greeting);
-                startActivityForResult(intent, SHOW_OTHER_ACTIVITY);
+        greetingButton.setOnClickListener {
+            val greeting = "Привет"
+            val intent = Intent(this, SecondActivity::class.java).apply {
+                putExtra("greeting", greeting)
             }
-        });
+            startActivityForResult(intent, SHOW_OTHER_ACTIVITY)
+        }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == SHOW_OTHER_ACTIVITY && resultCode == RESULT_OK && data != null) {
-            String name = data.getStringExtra("name");
-            greetingTextView = findViewById(R.id.greetingTextView);
-            greetingTextView.setText("Привет, " + name + "!");
+    @SuppressLint("SetTextI18n")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SHOW_OTHER_ACTIVITY && resultCode == RESULT_OK) {
+            val name = data?.getStringExtra("name")
+            greetingTextView.text = "Привет, $name!"
         }
+    }
+
+    companion object {
+        private const val SHOW_OTHER_ACTIVITY = 1
     }
 }
--- app/src/main/java/com/example/helloandriod/SecondActivity.kt
+++ app/src/main/java/com/example/helloandriod/SecondActivity.kt
@@ -1,36 +1,30 @@
-package com.example.helloandriod;
+package com.example.helloandriod
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
+class SecondActivity : AppCompatActivity() {
+    private lateinit var enteredName: EditText
+    private lateinit var submitButton: Button
 
-public class SecondActivity extends AppCompatActivity {
-    private EditText enteredName;
-    private Button submitButton;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        enteredName = findViewById(R.id.enteredName)
+        submitButton = findViewById(R.id.submitButton)
 
-        enteredName = findViewById(R.id.enteredName);
-        submitButton = findViewById(R.id.submitButton);
-
-        String greetingMessage = getIntent().getStringExtra("greeting");
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = enteredName.getText().toString();
-                Intent intent = new Intent();
-                intent.putExtra("name", name);
-                setResult(RESULT_OK, intent);
-                finish();
+        submitButton.setOnClickListener {
+            val name = enteredName.text.toString()
+            val intent = Intent().apply {
+                putExtra("name", name)
             }
-        });
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
